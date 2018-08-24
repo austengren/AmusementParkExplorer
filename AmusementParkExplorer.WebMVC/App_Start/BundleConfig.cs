2f190994195a443675ac9d51592b4afccfcdd5ff
Austen Gren <austenbgren@gmail.com>
2018-08-24 10:26:16 -0400

    first UI changes

--- AmusementParkExplorer.WebMVC/App_Start/BundleConfig.cs
+++ AmusementParkExplorer.WebMVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace AmusementParkExplorer.WebMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-simplex.css",
                       "~/Content/site.css"));
         }
     }
